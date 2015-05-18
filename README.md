01b41c213a685c7e3d92d933fc086c80d9af9317
john layton <johnstewartlayton@gmail.com>
2015-05-18 13:31:14 +1000

    update the client caching esri leaflet plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+'''
+    "esri-leaflet"          : "https://github.com/Esri/esri-leaflet/releases/download/v1.0.0-rc.4/esri-leaflet-v1.0.0-rc.4.zip",
+'''
--- leaflet-esri-feature-layer.js
+++ leaflet-esri-feature-layer.js
@@ -2,16 +2,59 @@
 
 Polymer( 'leaflet-esri-feature-layer', {
 
+  observe: {
+    'container storage': 'containerChanged'
+  },
+
   created : function () {
   },
 
   ready : function () {
   },
 
+  add : function( key, data ) {
+    this.storage.add( key, data );
+  },
+
+  get : function( key, callback, fallback ) {
+    this.storage.get( key, function( value ) {
+      if ( value ) {
+        callback( value );
+      }
+      else {
+        fallback();
+      }
+    }, function() {
+      fallback();
+    } );
+  },
+
+  requestFeatures : function( context ) {
+    var original = context._requestFeatures;
+    return function( bounds, coords, requestCallback ) {
+      var args = arguments;
+      this.get( JSON.stringify( coords ), function( value ) {
+        var featureCollection = value;
+        if ( featureCollection.features.length ) {
+          context._addFeatures( featureCollection.features, coords );
+        }
+        if ( requestCallback ) {
+          requestCallback.call( context, error, featureCollection );
+        }
+      }.bind( this ), function() {
+        original.apply( context, [ bounds, coords, function( err, value ) {
+          this.add( JSON.stringify( coords ), value );
+        }.bind( this ) ] );
+      }.bind( this ) )
+    }.bind( this );
+  },
+
   containerChanged : function () {
-    if ( this.container ) {
+
+    if ( this.container && this.storage ) {
       var icon = this.icon;
       var url = this.url;
+
       this.layer = L.esri.featureLayer( url, {
         pointToLayer: function (geojson, latlng) {
           function trim( text ) {
@@ -38,6 +81,7 @@ Polymer( 'leaflet-esri-feature-layer', {
           }).bindPopup( "<div><dl class='multiple-table'><dt>" + details.join("</dd><dt>") +"</dd></dl><div>" );
         }
       });
+      this.layer._requestFeatures = this.requestFeatures( this.layer );
       this.container.addLayer( this.layer );
     }
   },
